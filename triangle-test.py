1dc7815e87e865a078c76992079bbb01128f66d6
valbeat <kj1ktk@gmail.com>
2017-10-22 00:01:45 +0900

    [update] unittestのテスト

--- triangle-test.py
+++ triangle-test.py
@@ -8,12 +8,18 @@
     - [] 三角形でないときは例外
 """
 
-
-def main():
-    """
-        Entry Point
-    """
+import unittest
 
 
-if __name__ == "__main__"():
-    main()
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+
+if __name__ == "__main__":
+    unittest.main()
